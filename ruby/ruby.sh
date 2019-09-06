5802f3209898dd64da8958b39650411de567f087
Alysson A. Costa <alysson.avila.costa@gmail.com>
2019-09-06 13:32:07 -0400

    Add Vagrantfile and script to automate box creation for ruby environment

---
+++ ruby/ruby.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && sudo apt-get install gnupg2 -y
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install 2.5.1
+rvm use 2.5.1 --default
